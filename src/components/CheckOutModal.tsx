d86a65e1d5b42f61384c4b11f59ed0dcfed62b3a
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-22 19:40:58 +0600

    refactor: remove unused axios dependency and handleSubmit function from CheckOutModal

--- src/components/CheckOutModal.tsx
+++ src/components/CheckOutModal.tsx
@@ -1,19 +1,9 @@
 "use client"
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import axios from 'axios';
 import CheckoutForm from './CheckoutForm';
 export default function CheckOutModal({ amount, frequency, firstName, lastName, notifySomeone, setIsModalOpen }: { amount: string, frequency: string, firstName: string, lastName: string, notifySomeone: boolean, setIsModalOpen: (value: boolean) => void }) {
     const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
-    const handelSubmit = async () => {
-        const res = await axios.post('/api/payment', {
-            amount: amount,
-            frequency: frequency,
-            firstName: firstName,
-            lastName: lastName,
-            notifySomeone: notifySomeone
-        })
-    }
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-black/10 backdrop-blur-md bg-opacity-50 flex justify-center items-center">
             <div className="bg-white p-8  w-full max-w-md rounded-xl">
